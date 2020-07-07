788cc8688f20f21964337e2abec1d0b67a259d9c
Alexandru Suilea <42109259+AlexSuilea@users.noreply.github.com>
2020-07-08 01:04:17 +0300

    Add files via upload

---
+++ Overloading_FeetAndInchesToCentimeters/src/com/alexsuilea/Main.java
@@ -0,0 +1,8 @@
+package com.alexsuilea;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(FeetAndInchesToCentimeters.calcFeetAndInchesToCentimeters(157));
+    }
+}
